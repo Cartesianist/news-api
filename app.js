e7a4b363b1751135804e900da07068a1f41c6574
Ibraheem <ibraheemalmadini@gmail.com>
2022-09-06 11:36:08 +0100

    Ticket 3

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const { getTopics } = require('./controllers/topics.controllers');
+
+
+app.get('/api/topics', getTopics);
+
+app.use((err, req, res, next) => {
+    console.log(err, "<< uncaught error");
+    res.status(500).send({ msg: "internal server error" });
+});
+
+module.exports = app;
