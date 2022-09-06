82b6cc4592e6490c937e51bb4568bd1d1532615c
Ibraheem <ibraheemalmadini@gmail.com>
2022-09-06 16:00:40 +0100

    Ticket 4

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require('./controllers/topics.controllers');
+const { getArticle } = require('./controllers/articles.controllers');
+
 
 
 app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticle)
+
+app.use((err, req, res, next) => {
+    const badRequestCodes = ['22P02']
+    if (badRequestCodes.includes(err.code)) {
+        res.status(400).send({ msg: "Invalid input" })
+    }
+    else {
+        next(err);
+    };
+});
+
+app.use((err, req, res, next) => {
+    if (err.msg && err.status) {
+        res.status(err.status).send({ msg: err.msg });
+    } else {
+        next(err);
+    }
+});
 
 app.use((err, req, res, next) => {
     console.log(err, "<< uncaught error");
---
+++ controllers/articles.controllers.js
@@ -0,0 +1,10 @@
+const { fetchArticle } = require('../models/articles.models');
+
+exports.getArticle = (req, res, next) => {
+    const { article_id } = req.params;
+    return fetchArticle(article_id)
+        .then((article) => {
+            res.status(200).send({ article })
+        })
+        .catch(next);
+};;
